b6d6b195fe926aba0d85bb4550825e30fb81b0e2
ashu-158cm <je38516240263t@icloud.com>
2024-09-20 04:50:17 +0000

    9月19日分を別ファイルにいどう

---
+++ 9_19/main.js
@@ -0,0 +1,19 @@
+document.querySelector("h1").textContent="たなべあしゅう"
+
+
+//documentとは
+//htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。
+
+//DOM
+//ドキュメントオブジェクトモデル（Document object model）の略で
+//webページの要素やコンテンツなどをツリー構造で表現したデータモデル
+
+//上記のコードの意味
+//docment.querySelector("h1").textContent="たなべあしゅう"
+
+//ドキュメントの中の（”h1”）タグを探してね
+//document.querySelector("h1")
+//h1タグの中身をtextを変えてね
+//.textcontent="たなべあしゅう"
+
+document.querySelector("h2").textContent="たなべあしゅう"
