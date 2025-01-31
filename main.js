de7a2ccff4a9e8aab93ba3cdff0ccec4ec2b184f
ashu-158cm <je38516240263t@icloud.com>
2025-01-31 06:18:22 +0000

    せーれーくたーのついかー

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
-// function ClickCounterGame(){
-//     let count = 0;
-//     //countという変数を準備して、0を代入する
-//     const gameContainer = document.getElementById("game-container");
-//     //constは定数　変更不可　htmlの特定のidのタグを探し、定数に代入する
+ const gameSelector = document.getElementById('game-select');
+ const gameContainer = document.getElementById("game-container");
+ gameSelector.addEventListener("change",function(){
+    gameContainer.innerHTML="";
+    switch (gameSelector.value){
+        case "number-guess":
+            startNumberGuessgame();
+            break;
+            case "clicker":
+                ClickCounterGame();
+                break;
+    }
+ });
 
-//     let button1 = document.createElement("button");
-//     let button2 = document.createElement("button");
-//     let button3 = document.createElement("button");
-//     //ボタンのタグを作成する　<button>ボタン</button>を作り、変数と紐づける➀
-//     button1.textContent = "+1ボタン";
-//     button2.textContent = "+10ボタン";
-//     button3.textContent = "リセット";
-//    //<button>中身の文字をtextContentという</button>
+ function ClickCounterGame(){
+     let count = 0;
+     //countという変数を準備して、0を代入する
+     const gameContainer = document.getElementById("game-container");
+     //constは定数　変更不可　htmlの特定のidのタグを探し、定数に代入する
 
-//     let counter = document.createElement("p");
-//     counter.textContent = count;
+     let button1 = document.createElement("button");
+     let button2 = document.createElement("button");
+     let button3 = document.createElement("button");
+     //ボタンのタグを作成する　<button>ボタン</button>を作り、変数と紐づける➀
+     button1.textContent = "+1ボタン";
+     button2.textContent = "+10ボタン";
+     button3.textContent = "リセット";
+    //<button>中身の文字をtextContentという</button>
 
-//     button1.addEventListener("click",function(){//クリックというイベントを読みとる➁
-//     if(count<100)count++;
-//         counter.textContent = count;
-//     })
-//      button2.addEventListener("click",function(){//クリックというイベントを読みとる➁
-//        if(count>90) count=100;
-//         if(count<91)  count=count+10;
-//         counter.textContent = count;
-//         //イベントが起きた場合の処理を記述 ➂
-//            })
-//            button3.addEventListener("click",function(){//クリックというイベントを読みとる➁
-//             count=0;
-//             counter.textContent = count;
-//            })
-//         gameContainer.appendChild(button1);//ボタンを表示する　➃
-//         gameContainer.appendChild(button2);
-//         gameContainer.appendChild(button3);
-//         gameContainer.appendChild(counter);
-// }
-// ClickCounterGame();
+     let counter = document.createElement("p");
+     counter.textContent = count;
+
+     button1.addEventListener("click",function(){//クリックというイベントを読みとる➁
+     if(count<100)count++;
+         counter.textContent = count;
+     })
+    button2.addEventListener("click",function(){//クリックというイベントを読みとる➁
+       if(count>90) count=100;
+         if(count<91)  count=count+10;
+         counter.textContent = count;
+         //イベントが起きた場合の処理を記述 ➂
+            })
+            button3.addEventListener("click",function(){//クリックというイベントを読みとる➁
+             count=0;
+             counter.textContent = count;
+            })
+         gameContainer.appendChild(button1);//ボタンを表示する　➃
+         gameContainer.appendChild(button2);
+         gameContainer.appendChild(button3);
+         gameContainer.appendChild(counter);
+ }
+ 
 function startNumberGuessgame(){
     const gameContainer = document.getElementById("game-container");
     const randomNumber = Math.floor(Math.random() * 100) +1;
@@ -75,4 +89,4 @@ function startNumberGuessgame(){
     gameContainer.appendChild(button);
     gameContainer.appendChild(counter);
 }
-startNumberGuessgame();
+
